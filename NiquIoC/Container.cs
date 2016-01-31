4da89f08fafa8e643d37af4146ee6def98b807ed
Adrian Mularczyk <adrian.mularczyk@hotmail.com>
2016-01-31 14:11:21 +0100

    Testing MethodInvoker.

--- NiquIoC/Container.cs
+++ NiquIoC/Container.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,28 +10,116 @@ namespace NiquIoC
 {
     public class Container : IContainer
     {
-        #region IContainer Members
+        Dictionary<Type, Tuple<bool, object>> _classContainer;
+        Dictionary<Type, Tuple<bool, Type, object>> _interfaceContainer;
 
-        public void RegisterType<T>(bool singleton) where T : class
+        public Container()
         {
-            throw new NotImplementedException();
+            _classContainer = new Dictionary<Type, Tuple<bool, object>>();
+            _interfaceContainer = new Dictionary<Type, Tuple<bool, Type, object>>();
         }
 
-        public void RegisterType<From, To>(bool singleton) where To : From
+        #region IContainer Members
+
+        public void RegisterType<T>(bool singleton)
+            where T : class
         {
-            throw new NotImplementedException();
+            if (!_classContainer.ContainsKey(typeof(T)))
+            {
+                _classContainer.Add(typeof(T), Tuple.Create(singleton, (object)null));
+            }
+            else
+            {
+                _classContainer[typeof(T)] = Tuple.Create(singleton, (object)null);
+            }
+        }
+
+        public void RegisterType<From, To>(bool singleton)
+            where To : From
+        {
+            if (!_interfaceContainer.ContainsKey(typeof(From)))
+            {
+                _interfaceContainer.Add(typeof(From), Tuple.Create(singleton, typeof(To), (object)null));
+            }
+            else
+            {
+                _interfaceContainer[typeof(From)] = Tuple.Create(singleton, typeof(To), (object)null);
+            }
         }
 
         public void RegisterInstance<T>(T instance)
         {
-            throw new NotImplementedException();
+            if (typeof(T).IsInterface)
+            {
+                if (!_interfaceContainer.ContainsKey(typeof(T)))
+                {
+                    _interfaceContainer.Add(typeof(T), Tuple.Create(true, instance.GetType(), (object)instance));
+                }
+                else
+                {
+                    _interfaceContainer[typeof(T)] = Tuple.Create(true, instance.GetType(), (object)instance);
+                }
+            }
+            else
+            {
+                if (!_classContainer.ContainsKey(typeof(T)))
+                {
+                    _classContainer.Add(typeof(T), Tuple.Create(true, (object)instance));
+                }
+                else
+                {
+                    _classContainer[typeof(T)] = Tuple.Create(true, (object)instance);
+                }
+            }
         }
 
         public T Resolve<T>()
         {
-            throw new NotImplementedException();
+            var result = _interfaceContainer[typeof(T)];
+            return (T)CreateInstance(result.Item2);
         }
 
         #endregion
+
+        private object CreateInstance(Type type)
+        {
+            return CreateInstance2(type);
+        }
+
+        private object CreateInstance1(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+
+        delegate object MethodInvoker();
+        MethodInvoker methodHandler = null;
+        bool _created = false;
+        
+        void CreateMethod(Type type)
+        {
+            ConstructorInfo target = type.GetConstructor(Type.EmptyTypes);
+            DynamicMethod dynamic = new DynamicMethod(string.Empty,
+                        typeof(object),
+                        new Type[0],
+                        target.DeclaringType);
+            ILGenerator il = dynamic.GetILGenerator();
+            il.DeclareLocal(target.DeclaringType);
+            il.Emit(OpCodes.Newobj, target);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Ret);
+
+            methodHandler = (MethodInvoker)dynamic.CreateDelegate(typeof(MethodInvoker));
+        }
+
+        public object CreateInstance2(Type type)
+        {
+            if (!_created)
+            {
+                CreateMethod(type);
+                _created = true;
+            }
+            return methodHandler();
+        }
     }
 }
--- PerformanceTests/ClassATests.cs
+++ PerformanceTests/ClassATests.cs
@@ -3,9 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SampleContainer;
 using PerformanceTests.Classes;
 using System.Diagnostics;
-using Hiro;
-using Hiro.Containers;
 using System.Linq;
+using NiquIoC;
 
 namespace PerformanceTests
 {
@@ -76,10 +75,55 @@ namespace PerformanceTests
             Assert.IsNotNull(test_a10.Test_a9.Test_a8.Test_a7.Test_a6.Test_a4.Test_a3.Test_a2.Test_a1.Test_a0);
         }
 
+
+        [TestMethod]
+        public void NiquIoCTest()
+        {
+            NiquIoC.IContainer c = new NiquIoC.Container();
+
+            Stopwatch sw = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
+            sw.Start();
+            c.RegisterType<ITest_A0, Test_A0>(false);
+            //c.RegisterType<ITest_A1, Test_A1>(false);
+            //c.RegisterType<ITest_A2, Test_A2>(false);
+            //c.RegisterType<ITest_A3, Test_A3>(false);
+            //c.RegisterType<ITest_A4, Test_A4>(false);
+            //c.RegisterType<ITest_A5, Test_A5>(false);
+            //c.RegisterType<ITest_A6, Test_A6>(false);
+            //c.RegisterType<ITest_A7, Test_A7>(false);
+            //c.RegisterType<ITest_A8, Test_A8>(false);
+            //c.RegisterType<ITest_A9, Test_A9>(false);
+            //c.RegisterType<ITest_A10, Test_A10>(false);
+            sw.Stop();
+
+            Debug.WriteLine(string.Format("Register: {0} Ticks.", sw.ElapsedTicks));
+            sw.Reset();
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Start();
+                //var test = c.Resolve<ITest_A10>();
+                var test = c.Resolve<ITest_A0>();
+                sw.Stop();
+
+                sw2.Start();
+                Assert.IsNotNull(test);
+                sw2.Stop();
+            }
+
+            Debug.WriteLine(string.Format("Resolve: {0} Ticks.", sw.ElapsedTicks));
+            Debug.WriteLine(string.Format("Resolve: {0} Milliseconds.", sw.ElapsedMilliseconds));
+
+            Debug.WriteLine(string.Format("Checking: {0} Ticks.", sw2.ElapsedTicks));
+            Debug.WriteLine(string.Format("Checking: {0} Milliseconds.", sw2.ElapsedMilliseconds));
+        }
+
         [TestMethod]
         public void UpperIntermediateContainerTest()
         {
-            IContainer c = new UpperIntermediateContainer();
+            SampleContainer.IContainer c = new SampleContainer.UpperIntermediateContainer();
 
             Stopwatch sw = new Stopwatch();
             Stopwatch sw2 = new Stopwatch();
@@ -122,7 +166,7 @@ namespace PerformanceTests
         [TestMethod]
         public void IntermediateContainerTest()
         {
-            IContainer c = new IntermediateContainer();
+            SampleContainer.IContainer c = new SampleContainer.IntermediateContainer();
 
             Stopwatch sw = new Stopwatch();
             Stopwatch sw2 = new Stopwatch();
@@ -162,48 +206,48 @@ namespace PerformanceTests
             Debug.WriteLine(string.Format("Checking: {0} Milliseconds.", sw2.ElapsedMilliseconds));
         }
 
-        [TestMethod]
-        public void HiroTest()
-        {
-            var dependencyMap = new DependencyMap();
+        //[TestMethod]
+        //public void HiroTest()
+        //{
+        //    var dependencyMap = new DependencyMap();
 
-            Stopwatch sw = new Stopwatch();
-            Stopwatch sw2 = new Stopwatch();
+        //    Stopwatch sw = new Stopwatch();
+        //    Stopwatch sw2 = new Stopwatch();
 
-            sw.Start();
-            dependencyMap.AddService<ITest_A0, Test_A0>();
-            dependencyMap.AddService<ITest_A1, Test_A1>();
-            dependencyMap.AddService<ITest_A2, Test_A2>();
-            dependencyMap.AddService<ITest_A3, Test_A3>();
-            dependencyMap.AddService<ITest_A4, Test_A4>();
-            dependencyMap.AddService<ITest_A5, Test_A5>();
-            dependencyMap.AddService<ITest_A6, Test_A6>();
-            dependencyMap.AddService<ITest_A7, Test_A7>();
-            dependencyMap.AddService<ITest_A8, Test_A8>();
-            dependencyMap.AddService<ITest_A9, Test_A9>();
-            dependencyMap.AddService<ITest_A10, Test_A10>();
-            sw.Stop();
+        //    sw.Start();
+        //    dependencyMap.AddService<ITest_A0, Test_A0>();
+        //    dependencyMap.AddService<ITest_A1, Test_A1>();
+        //    dependencyMap.AddService<ITest_A2, Test_A2>();
+        //    dependencyMap.AddService<ITest_A3, Test_A3>();
+        //    dependencyMap.AddService<ITest_A4, Test_A4>();
+        //    dependencyMap.AddService<ITest_A5, Test_A5>();
+        //    dependencyMap.AddService<ITest_A6, Test_A6>();
+        //    dependencyMap.AddService<ITest_A7, Test_A7>();
+        //    dependencyMap.AddService<ITest_A8, Test_A8>();
+        //    dependencyMap.AddService<ITest_A9, Test_A9>();
+        //    dependencyMap.AddService<ITest_A10, Test_A10>();
+        //    sw.Stop();
 
-            Debug.WriteLine(string.Format("Register: {0} Ticks.", sw.ElapsedTicks));
-            sw.Reset();
+        //    Debug.WriteLine(string.Format("Register: {0} Ticks.", sw.ElapsedTicks));
+        //    sw.Reset();
 
-            for (int i = 0; i < 100; i++)
-            {
-                sw.Start();
-                var dependency = new Dependency(typeof(ITest_A10));
-                var test = dependencyMap.CreateContainer().GetInstance<ITest_A10>();
-                sw.Stop();
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        sw.Start();
+        //        var dependency = new Dependency(typeof(ITest_A10));
+        //        var test = dependencyMap.CreateContainer().GetInstance<ITest_A10>();
+        //        sw.Stop();
 
-                sw2.Start();
-                Check(test);
-                sw2.Stop();
-            }
+        //        sw2.Start();
+        //        Check(test);
+        //        sw2.Stop();
+        //    }
 
-            Debug.WriteLine(string.Format("Resolve: {0} Ticks.", sw.ElapsedTicks));
-            Debug.WriteLine(string.Format("Resolve: {0} Milliseconds.", sw.ElapsedMilliseconds));
+        //    Debug.WriteLine(string.Format("Resolve: {0} Ticks.", sw.ElapsedTicks));
+        //    Debug.WriteLine(string.Format("Resolve: {0} Milliseconds.", sw.ElapsedMilliseconds));
 
-            Debug.WriteLine(string.Format("Checking: {0} Ticks.", sw2.ElapsedTicks));
-            Debug.WriteLine(string.Format("Checking: {0} Milliseconds.", sw2.ElapsedMilliseconds));
-        }
+        //    Debug.WriteLine(string.Format("Checking: {0} Ticks.", sw2.ElapsedTicks));
+        //    Debug.WriteLine(string.Format("Checking: {0} Milliseconds.", sw2.ElapsedMilliseconds));
+        //}
     }
 }
