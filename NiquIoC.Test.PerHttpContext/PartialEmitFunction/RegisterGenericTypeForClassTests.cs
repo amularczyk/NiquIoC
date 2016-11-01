2cf55ecaaf4114a754bfe9f26a119f1690c37944
Adrian Mularczyk <adrian.mularczyk@hotmail.com>
2016-11-01 12:01:05 +0100

    Add some tests for generic class and interface for register as per http context.

---
+++ NiquIoC.Test.PerHttpContext/PartialEmitFunction/RegisterGenericTypeForClassTests.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiquIoC.Enums;
+using NiquIoC.Test.Model;
+using NiquIoC.Test.WebApplication.Controllers;
+
+namespace NiquIoC.Test.PerHttpContext.PartialEmitFunction
+{
+    [TestClass]
+    public class RegisterGenericTypeForClassTests
+    {
+        [TestMethod]
+        public void RegisterSimpleGenericClass_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<GenericClass<EmptyClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<GenericClass<EmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (GenericClass<EmptyClass>)((ViewResult)result).Model;
+
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass);
+        }
+
+        [TestMethod]
+        public void RegisterGenericClassWithClassWithNestedClass_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+            c.RegisterType<GenericClass<SampleClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<GenericClass<SampleClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (GenericClass<SampleClass>)((ViewResult)result).Model;
+            
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass);
+            Assert.IsNotNull(genericClass.NestedClass.EmptyClass);
+        }
+
+        [TestMethod]
+        public void RegisterGenericClassWithManyParameters_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+            c.RegisterType<GenericClassWithManyParameters<EmptyClass, SampleClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<GenericClassWithManyParameters<EmptyClass, SampleClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (GenericClassWithManyParameters<EmptyClass, SampleClass>)((ViewResult)result).Model;
+            
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass1);
+            Assert.IsNotNull(genericClass.NestedClass2);
+            Assert.IsNotNull(genericClass.NestedClass2.EmptyClass);
+            Assert.AreEqual(genericClass.NestedClass1, genericClass.NestedClass2.EmptyClass);
+        }
+
+        [TestMethod]
+        public void SameThread_RegisterManyGenericClasses_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+            c.RegisterType<GenericClass<EmptyClass>>().AsPerHttpContext();
+            c.RegisterType<GenericClass<SampleClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<GenericClass<EmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass1 = (GenericClass<EmptyClass>)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<GenericClass<SampleClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass2 = (GenericClass<SampleClass>)((ViewResult)result2).Model;
+            
+
+            Assert.AreNotEqual(genericClass1, genericClass2);
+            Assert.AreNotEqual(genericClass1.GetType(), genericClass2.GetType());
+            Assert.AreEqual(genericClass1.NestedClass, genericClass2.NestedClass.EmptyClass);
+            Assert.AreEqual(genericClass1.NestedClass.GetType(), genericClass2.NestedClass.EmptyClass.GetType());
+        }
+
+        [TestMethod]
+        public void DifferentThreads_RegisterManyGenericClasses_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+            c.RegisterType<GenericClass<EmptyClass>>().AsPerHttpContext();
+            c.RegisterType<GenericClass<SampleClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<GenericClass<EmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass1 = (GenericClass<EmptyClass>)((ViewResult)result1).Model;
+            
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result2 = controller.ResolveObject<GenericClass<SampleClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass2 = (GenericClass<SampleClass>)((ViewResult)result2).Model;
+            
+
+            Assert.AreNotEqual(genericClass1, genericClass2);
+            Assert.AreNotEqual(genericClass1.GetType(), genericClass2.GetType());
+            Assert.AreNotEqual(genericClass1.NestedClass, genericClass2.NestedClass.EmptyClass);
+            Assert.AreEqual(genericClass1.NestedClass.GetType(), genericClass2.NestedClass.EmptyClass.GetType());
+        }
+    }
+}
---
+++ NiquIoC.Test.PerHttpContext/PartialEmitFunction/RegisterGenericTypeForInterfaceTests.cs
@@ -0,0 +1,126 @@
+﻿using System.IO;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiquIoC.Enums;
+using NiquIoC.Test.Model;
+using NiquIoC.Test.WebApplication.Controllers;
+
+namespace NiquIoC.Test.PerHttpContext.PartialEmitFunction
+{
+    [TestClass]
+    public class RegisterGenericTypeForInterfaceTests
+    {
+        [TestMethod]
+        public void RegisterSimpleGenericClass_Success()
+        {
+            var c = new Container();
+            c.RegisterType<IEmptyClass, EmptyClass>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<IEmptyClass>, GenericClass<IEmptyClass>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<IGenericClass<IEmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (IGenericClass<IEmptyClass>)((ViewResult)result).Model;
+
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass);
+        }
+
+        [TestMethod]
+        public void RegisterGenericClassWithClassWithNestedClass_Success()
+        {
+            var c = new Container();
+            c.RegisterType<IEmptyClass, EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClassWithInterfaceAsParameter, SampleClassWithInterfaceAsParameter>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<ISampleClassWithInterfaceAsParameter>, GenericClass<ISampleClassWithInterfaceAsParameter>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<IGenericClass<ISampleClassWithInterfaceAsParameter>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (IGenericClass<ISampleClassWithInterfaceAsParameter>)((ViewResult)result).Model;
+            
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass);
+            Assert.IsNotNull(genericClass.NestedClass.EmptyClass);
+        }
+
+        [TestMethod]
+        public void RegisterGenericClassWithManyParameters_Success()
+        {
+            var c = new Container();
+            c.RegisterType<IEmptyClass, EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClassWithInterfaceAsParameter, SampleClassWithInterfaceAsParameter>().AsPerHttpContext();
+            c.RegisterType<IGenericClassWithManyParameters<IEmptyClass, ISampleClassWithInterfaceAsParameter>,
+                GenericClassWithManyParameters<IEmptyClass, ISampleClassWithInterfaceAsParameter>>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<IGenericClassWithManyParameters<IEmptyClass, ISampleClassWithInterfaceAsParameter>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass = (IGenericClassWithManyParameters<IEmptyClass, ISampleClassWithInterfaceAsParameter>)((ViewResult)result).Model;
+            
+
+            Assert.IsNotNull(genericClass);
+            Assert.IsNotNull(genericClass.NestedClass1);
+            Assert.IsNotNull(genericClass.NestedClass2);
+            Assert.IsNotNull(genericClass.NestedClass2.EmptyClass);
+            Assert.AreEqual(genericClass.NestedClass1, genericClass.NestedClass2.EmptyClass);
+        }
+
+        [TestMethod]
+        public void SameThread_RegisterManyGenericClasses_Success()
+        {
+            var c = new Container();
+            c.RegisterType<IEmptyClass, EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClassWithInterfaceAsParameter, SampleClassWithInterfaceAsParameter>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<IEmptyClass>, GenericClass<IEmptyClass>>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<ISampleClassWithInterfaceAsParameter>, GenericClass<ISampleClassWithInterfaceAsParameter>>().AsPerHttpContext();
+            
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<IGenericClass<IEmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass1 = (IGenericClass<IEmptyClass>)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<IGenericClass<ISampleClassWithInterfaceAsParameter>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass2 = (IGenericClass<ISampleClassWithInterfaceAsParameter>)((ViewResult)result2).Model;
+            
+
+            Assert.AreNotEqual(genericClass1, genericClass2);
+            Assert.AreNotEqual(genericClass1.GetType(), genericClass2.GetType());
+            Assert.AreEqual(genericClass1.NestedClass, genericClass2.NestedClass.EmptyClass);
+            Assert.AreEqual(genericClass1.NestedClass.GetType(), genericClass2.NestedClass.EmptyClass.GetType());
+        }
+
+        [TestMethod]
+        public void DifferentThreads_RegisterManyGenericClasses_Success()
+        {
+            var c = new Container();
+            c.RegisterType<IEmptyClass, EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClassWithInterfaceAsParameter, SampleClassWithInterfaceAsParameter>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<IEmptyClass>, GenericClass<IEmptyClass>>().AsPerHttpContext();
+            c.RegisterType<IGenericClass<ISampleClassWithInterfaceAsParameter>, GenericClass<ISampleClassWithInterfaceAsParameter>>().AsPerHttpContext();
+            
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<IGenericClass<IEmptyClass>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass1 = (IGenericClass<IEmptyClass>)((ViewResult)result1).Model;
+
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result2 = controller.ResolveObject<IGenericClass<ISampleClassWithInterfaceAsParameter>>(c, ResolveKind.PartialEmitFunction);
+            var genericClass2 = (IGenericClass<ISampleClassWithInterfaceAsParameter>)((ViewResult)result2).Model;
+            
+
+            Assert.AreNotEqual(genericClass1, genericClass2);
+            Assert.AreNotEqual(genericClass1.GetType(), genericClass2.GetType());
+            Assert.AreNotEqual(genericClass1.NestedClass, genericClass2.NestedClass.EmptyClass);
+            Assert.AreEqual(genericClass1.NestedClass.GetType(), genericClass2.NestedClass.EmptyClass.GetType());
+        }
+    }
+}
