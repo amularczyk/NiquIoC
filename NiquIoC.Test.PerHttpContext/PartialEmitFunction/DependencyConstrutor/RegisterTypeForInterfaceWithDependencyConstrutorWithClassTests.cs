6a7613962a9b54345b7a2b0c154459dd83e51746
Adrian Mularczyk <adrian.mularczyk@hotmail.com>
2016-12-08 09:40:30 +0100

    Add tests for DependencyConstructor, FactoryObject and ReRegister for registration as PerHttpContext for PartialEmitFunction.

---
+++ NiquIoC.Test.PerHttpContext/PartialEmitFunction/DependencyConstrutor/RegisterTypeForInterfaceWithDependencyConstrutorWithClassTests.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiquIoC.Enums;
+using NiquIoC.Exceptions;
+using NiquIoC.Test.Model;
+using NiquIoC.Test.WebApplication.Controllers;
+
+namespace NiquIoC.Test.PerHttpContext.PartialEmitFunction.DependencyConstrutor
+{
+    [TestClass]
+    public class RegisterTypeForInterfaceWithDependencyConstrutorWithClassTests
+    {
+        [TestMethod]
+        public void RegisteredInterfaceAsClassWithConstructorWithAttributeDependencyConstrutor_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClass, SampleClassWithDependencyConstrutor>().AsPerHttpContext();
+            
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<ISampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass = (ISampleClass)((ViewResult)result).Model;
+
+
+            Assert.IsNotNull(sampleClass);
+            Assert.IsNotNull(sampleClass.EmptyClass);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoProperConstructorException))]
+        public void RegisteredInterfaceAsClassWithTwoConstructorsWithAttributeDependencyConstrutor_Fail()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<ISampleClass, SampleClassWithTwoDependencyConstrutor>().AsPerHttpContext();
+            
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<ISampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass = (ISampleClass)((ViewResult)result).Model;
+
+
+            Assert.IsNull(sampleClass);
+        }
+        [TestMethod]
+        public void RegisteredInterfaceAsClassWithNestedClassAsParameterWithConstructorWithAttributeDependencyConstrutor_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>().AsPerHttpContext();
+            c.RegisterType<SampleClassWithDependencyConstrutor>().AsPerHttpContext();
+            c.RegisterType<ISampleClassWithNestedClass, SampleClassWithNestedClassWithDependencyConstrutor>().AsPerHttpContext();
+            
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result = controller.ResolveObject<ISampleClassWithNestedClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass = (ISampleClassWithNestedClass)((ViewResult)result).Model;
+
+
+            Assert.IsNotNull(sampleClass);
+            Assert.IsNotNull(sampleClass.SampleClassWithDependencyConstrutor);
+            Assert.IsNotNull(sampleClass.SampleClassWithDependencyConstrutor.EmptyClass);
+        }
+    }
+}
---
+++ NiquIoC.Test.PerHttpContext/PartialEmitFunction/FactoryObject/RegisterTypeByFactoryObjectForClassTests.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiquIoC.Enums;
+using NiquIoC.Test.Model;
+using NiquIoC.Test.WebApplication.Controllers;
+
+namespace NiquIoC.Test.PerHttpContext.PartialEmitFunction.FactoryObject
+{
+    [TestClass]
+    public class RegisterTypeByFactoryObjectForClassTests
+    {
+        [TestMethod]
+        public void FactoryObjectReturnNewObject_Success()
+        {
+            var c = new Container();
+            var emptyClass = new EmptyClass();
+            c.RegisterType<SampleClass>(() => new SampleClass(emptyClass)).AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass1 = (SampleClass)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass2 = (SampleClass)((ViewResult)result2).Model;
+
+
+            Assert.AreEqual(sampleClass1, sampleClass2);
+            Assert.AreEqual(emptyClass, sampleClass1.EmptyClass);
+            Assert.AreEqual(emptyClass, sampleClass2.EmptyClass);
+        }
+
+        [TestMethod]
+        public void FactoryObjectReturnTheSameObject_Success()
+        {
+            var c = new Container();
+            var emptyClass = new EmptyClass();
+            var sampleClass = new SampleClass(emptyClass);
+            c.RegisterType<SampleClass>(() => sampleClass).AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass1 = (SampleClass)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass2 = (SampleClass)((ViewResult)result2).Model;
+
+
+            Assert.AreEqual(sampleClass1, sampleClass2);
+            Assert.AreEqual(emptyClass, sampleClass1.EmptyClass);
+            Assert.AreEqual(emptyClass, sampleClass2.EmptyClass);
+        }
+
+        [TestMethod]
+        public void NestedFactoryObjectReturnNewObject_Success()
+        {
+            var c = new Container();
+            c.RegisterType<EmptyClass>(() => new EmptyClass()).AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass1 = (SampleClass)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass2 = (SampleClass)((ViewResult)result2).Model;
+
+
+            Assert.AreEqual(sampleClass1, sampleClass2);
+            Assert.AreEqual(sampleClass1.EmptyClass, sampleClass2.EmptyClass);
+        }
+
+        [TestMethod]
+        public void NestedFactoryObjectReturnTheSameObject_Success()
+        {
+            var c = new Container();
+            var emptyClass = new EmptyClass();
+            c.RegisterType<EmptyClass>(() => emptyClass).AsPerHttpContext();
+            c.RegisterType<SampleClass>().AsPerHttpContext();
+
+
+            var controller = new DefaultController();
+            HttpContext.Current = new HttpContext(new HttpRequest("", "http://tempuri.org", ""), new HttpResponse(new StringWriter()));
+            var result1 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass1 = (SampleClass)((ViewResult)result1).Model;
+            var result2 = controller.ResolveObject<SampleClass>(c, ResolveKind.PartialEmitFunction);
+            var sampleClass2 = (SampleClass)((ViewResult)result2).Model;
+
+
+            Assert.AreEqual(sampleClass1, sampleClass2);
+            Assert.AreEqual(sampleClass1.EmptyClass, sampleClass2.EmptyClass);
+        }
+    }
+}
