f52c9a77a6393daded20de842692777277e511f2
Adrian Mularczyk <xniqux@gmail.com>
2017-01-14 11:42:09 +0100

    Fix using in DryIoc WebApp tests.

--- PerformanceCalculator.WebApp.Tests/TestsDryIoc/TestCaseCTests.cs
+++ PerformanceCalculator.WebApp.Tests/TestsDryIoc/TestCaseCTests.cs
@@ -3,7 +3,7 @@ using System.Web;
 using System.Web.Mvc;
 using DryIoc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PerformanceCalculator.Containers.TestsAutofac;
+using PerformanceCalculator.Containers.TestsDryIoc;
 using PerformanceCalculator.Interfaces;
 using PerformanceCalculator.TestCases;
 using PerformanceCalculator.WebApp.DryIoc.Controllers;
