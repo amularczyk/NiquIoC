564a391dcbee52b7fe684d1220c75e6bc28c8654
amularczyk <xniqux@gmail.com>
2017-05-20 21:47:57 +0300

    Some refactoring in FullEmitFunctionResolver. Adding class FullEmitFunctionResult.

--- NiquIoC/Resolver/FullEmitFunctionResolver.cs
+++ NiquIoC/Resolver/FullEmitFunctionResolver.cs
@@ -11,7 +11,9 @@ namespace NiquIoC.Resolver
 {
     internal class FullEmitFunctionResolver : IResolver
     {
-        private readonly Dictionary<Type, Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>> _createFullEmitFunctionForConstructorCache;
+        private readonly Dictionary<Type, FullEmitFunctionResult>
+            _createFullEmitFunctionForConstructorCache;
+
         private readonly Dictionary<Type, ContainerMember> _registeredTypesCache;
         private int _registeredTypesCacheCount;
 
@@ -20,16 +22,20 @@ namespace NiquIoC.Resolver
         public FullEmitFunctionResolver(Dictionary<Type, ContainerMember> registeredTypesCache)
         {
             _registeredTypesCache = registeredTypesCache;
-            _typesIndexCache = _registeredTypesCache.ToDictionary(k => k.Value.GetHashCode(), v => v.Key);
+            _typesIndexCache =
+                _registeredTypesCache.ToDictionary(k => k.Value.GetHashCode(), v => v.Key);
             _registeredTypesCacheCount = registeredTypesCache.Count;
-            _createFullEmitFunctionForConstructorCache = new Dictionary<Type, Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>>();
+            _createFullEmitFunctionForConstructorCache =
+                new Dictionary<Type, FullEmitFunctionResult>();
         }
 
-        public object Resolve(ContainerMember containerMember, Action<object, ContainerMember> afterObjectCreate)
+        public object Resolve(ContainerMember containerMember,
+            Action<object, ContainerMember> afterObjectCreate)
         {
             if (containerMember.ObjectLifetimeManager.ObjectFactory == null)
             {
-                containerMember.ObjectLifetimeManager.ObjectFactory = () => CreateInstanceFunction(containerMember, afterObjectCreate);
+                containerMember.ObjectLifetimeManager.ObjectFactory =
+                    () => GetObject(containerMember, afterObjectCreate);
             }
 
             return containerMember.ObjectLifetimeManager.GetInstance();
@@ -43,91 +49,138 @@ namespace NiquIoC.Resolver
             }
         }
 
-        private object CreateInstanceFunction(ContainerMember containerMember, Action<object, ContainerMember> afterObjectCreate)
+        private object GetObject(ContainerMember containerMember,
+            Action<object, ContainerMember> afterObjectCreate)
         {
-            if (!_createFullEmitFunctionForConstructorCache.ContainsKey(containerMember.ReturnType)) //if we do not have a create object function in the cache, we create it
-            {
-                var factoryMethod = CreateObjectFunction(containerMember, _registeredTypesCache, afterObjectCreate);
-                _createFullEmitFunctionForConstructorCache.Add(containerMember.ReturnType, factoryMethod);
-            }
-
-            var registeredTypesCacheCount = _registeredTypesCache.Count;
-            if (_registeredTypesCacheCount != registeredTypesCacheCount)
-            {
-                _typesIndexCache = _registeredTypesCache.ToDictionary(k => k.Value.GetHashCode(), v => v.Key);
-                _registeredTypesCacheCount = registeredTypesCacheCount;
-            }
-
-            var obj = _createFullEmitFunctionForConstructorCache[containerMember.ReturnType](_registeredTypesCache, _typesIndexCache);
-            afterObjectCreate(obj, containerMember); //when we have a new instance of the type, we have to resolve the properties and the methods also
+            var obj = CreateInstanceFunction(containerMember);
+            //when we have a new instance of the type, we have to resolve the properties and the methods also
+            afterObjectCreate(obj, containerMember);
 
             return obj;
         }
 
-        private Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object> CreateObjectFunction(ContainerMember containerMember,
-            IReadOnlyDictionary<Type, ContainerMember> registeredTypesCache, Action<object, ContainerMember> afterObjectCreate)
+        private object CreateInstanceFunction(ContainerMember containerMember)
         {
-            var dm = new DynamicMethod($"Create_{containerMember.Constructor.DeclaringType?.FullName.Replace('.', '_')}",
-                typeof(object), new[] { typeof(Dictionary<Type, ContainerMember>), typeof(Dictionary<int, Type>) }, typeof(Container).Module, true);
+            //if we do not have a create object function in the cache, we create it
+            if (!_createFullEmitFunctionForConstructorCache.ContainsKey(containerMember.ReturnType))
+            {
+                var factoryMethod = CreateObjectFunction(containerMember, _registeredTypesCache);
+                _createFullEmitFunctionForConstructorCache.Add(
+                    containerMember.ReturnType, factoryMethod);
+            }
+
+            ValidateTypesCache();
+
+            var obj =
+                _createFullEmitFunctionForConstructorCache[containerMember.ReturnType]
+                    .Result(_registeredTypesCache, _typesIndexCache);
+
+            return obj;
+        }
+
+        private void ValidateTypesCache()
+        {
+            var registeredTypesCacheCount = _registeredTypesCache.Count;
+            if (_registeredTypesCacheCount != registeredTypesCacheCount)
+            {
+                _typesIndexCache =
+                    _registeredTypesCache.ToDictionary(k => k.Value.GetHashCode(), v => v.Key);
+                _registeredTypesCacheCount = registeredTypesCacheCount;
+            }
+        }
+
+        private FullEmitFunctionResult CreateObjectFunction(ContainerMember containerMember,
+            IReadOnlyDictionary<Type, ContainerMember> registeredTypesCache)
+        {
+            var dm = new DynamicMethod(
+                $"Create_{containerMember.Constructor.DeclaringType?.FullName.Replace('.', '_')}",
+                typeof(object),
+                new[] {typeof(Dictionary<Type, ContainerMember>), typeof(Dictionary<int, Type>)},
+                typeof(Container).Module, true);
             var ilgen = dm.GetILGenerator();
 
             foreach (var parameter in containerMember.Parameters)
             {
-                CreateObjectFunctionPrivate(parameter.ParameterType, registeredTypesCache, ilgen, afterObjectCreate, new Dictionary<Type, LocalBuilder>());
+                CreateObjectFunctionPrivate(parameter.ParameterType, registeredTypesCache, ilgen,
+                    new Dictionary<Type, LocalBuilder>());
             }
             ilgen.Emit(OpCodes.Newobj, containerMember.Constructor);
             ilgen.Emit(OpCodes.Ret);
 
-            return (Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>)dm.CreateDelegate(typeof(Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>));
+            var func = dm.CreateDelegate(
+                typeof(Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>));
+            return new FullEmitFunctionResult
+            {
+                Result =
+                    (Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object>)func
+            };
         }
 
-        private void CreateObjectFunctionPrivate(Type type, IReadOnlyDictionary<Type, ContainerMember> registeredTypesCache, ILGenerator ilgen, Action<object, ContainerMember> afterObjectCreate,
+        private void CreateObjectFunctionPrivate(Type type,
+            IReadOnlyDictionary<Type, ContainerMember> registeredTypesCache, ILGenerator ilgen,
             IDictionary<Type, LocalBuilder> localSingletons)
         {
             var containerMember = registeredTypesCache.GetValue(type);
 
-            if (containerMember.ObjectLifetimeManager is TransientObjectLifetimeManager
-                && containerMember.ShouldCreateCache)
+            if (IsTransient(containerMember) && containerMember.ShouldCreateCache)
             {
                 foreach (var parameter in containerMember.Parameters)
                 {
-                    CreateObjectFunctionPrivate(parameter.ParameterType, registeredTypesCache, ilgen, afterObjectCreate, localSingletons);
+                    CreateObjectFunctionPrivate(parameter.ParameterType, registeredTypesCache,
+                        ilgen, localSingletons);
                 }
 
                 ilgen.Emit(OpCodes.Newobj, containerMember.Constructor);
             }
-            else if ((containerMember.ObjectLifetimeManager is SingletonObjectLifetimeManager
-                      || containerMember.ObjectLifetimeManager is ThreadObjectLifetimeManager
-                      || containerMember.ObjectLifetimeManager is HttpContextObjectLifetimeManager)
-                     && containerMember.ShouldCreateCache)
+            else if (IsSingleton(containerMember) && containerMember.ShouldCreateCache)
             {
                 if (localSingletons.ContainsKey(type))
                 {
-                    ilgen.Emit(OpCodes.Ldloc, localSingletons[type]); //when we created local variable that store our object, than we just put this object onto stack
+                    //when we created local variable that store our object, than we just put this object onto stack
+                    ilgen.Emit(OpCodes.Ldloc, localSingletons[type]);
                 }
                 else
                 {
-                    var localSingleton = ilgen.DeclareLocal(type); //we create local variable (this local variable will contain our object)
-                    localSingletons.Add(type, localSingleton); //we add this local variable into cache
+                    //we create local variable (this local variable will contain our object)
+                    var localSingleton = ilgen.DeclareLocal(type);
+                    //we add this local variable into cache
+                    localSingletons.Add(type, localSingleton);
 
-                    AddObjectCreatedByObjectLifetimeManager(type, ilgen, afterObjectCreate, containerMember);
+                    AddObjectCreatedByObjectLifetimeManager(type, ilgen, containerMember);
 
-                    var localVariable = localSingletons[type]; //we take local variable from cache
-                    ilgen.Emit(OpCodes.Stloc, localVariable); //we store value in local variable
-                    ilgen.Emit(OpCodes.Ldloc, localVariable); //we put value from local variable onto stack
+                    //we take local variable from cache
+                    var localVariable = localSingletons[type];
+                    //we store value in local variable
+                    ilgen.Emit(OpCodes.Stloc, localVariable);
+                    //we put value from local variable onto stack
+                    ilgen.Emit(OpCodes.Ldloc, localVariable);
                 }
             }
             else
             {
-                AddObjectCreatedByObjectLifetimeManager(type, ilgen, afterObjectCreate, containerMember);
+                AddObjectCreatedByObjectLifetimeManager(type, ilgen, containerMember);
             }
         }
 
-        private void AddObjectCreatedByObjectLifetimeManager(Type type, ILGenerator ilgen, Action<object, ContainerMember> afterObjectCreate, ContainerMember containerMember)
+        private static bool IsTransient(ContainerMember containerMember)
+        {
+            return containerMember.ObjectLifetimeManager is TransientObjectLifetimeManager;
+        }
+
+        private static bool IsSingleton(ContainerMember containerMember)
+        {
+            return containerMember.ObjectLifetimeManager is SingletonObjectLifetimeManager ||
+                   containerMember.ObjectLifetimeManager is ThreadObjectLifetimeManager ||
+                   containerMember.ObjectLifetimeManager is HttpContextObjectLifetimeManager;
+        }
+
+        private void AddObjectCreatedByObjectLifetimeManager(Type type, ILGenerator ilgen,
+            ContainerMember containerMember)
         {
             if (containerMember.ObjectLifetimeManager.ObjectFactory == null)
             {
-                containerMember.ObjectLifetimeManager.ObjectFactory = () => CreateInstanceFunction(containerMember, afterObjectCreate);
+                containerMember.ObjectLifetimeManager.ObjectFactory =
+                    () => CreateInstanceFunction(containerMember);
             }
 
             var localTypeVariable = ilgen.DeclareLocal(typeof(Type));
@@ -137,9 +190,12 @@ namespace NiquIoC.Resolver
             ilgen.Emit(OpCodes.Stloc, localTypeVariable);
             ilgen.Emit(OpCodes.Ldarg_0);
             ilgen.Emit(OpCodes.Ldloc, localTypeVariable);
-            ilgen.Emit(OpCodes.Call, typeof(Dictionary<Type, ContainerMember>).GetMethod("get_Item"));
-            ilgen.Emit(OpCodes.Call, typeof(ContainerMember).GetMethod("get_ObjectLifetimeManager", Type.EmptyTypes));
-            ilgen.Emit(OpCodes.Call, typeof(IObjectLifetimeManager).GetMethod("GetInstance", Type.EmptyTypes));
+            ilgen.Emit(OpCodes.Call,
+                typeof(Dictionary<Type, ContainerMember>).GetMethod("get_Item"));
+            ilgen.Emit(OpCodes.Call,
+                typeof(ContainerMember).GetMethod("get_ObjectLifetimeManager", Type.EmptyTypes));
+            ilgen.Emit(OpCodes.Call,
+                typeof(IObjectLifetimeManager).GetMethod("GetInstance", Type.EmptyTypes));
             ilgen.Emit(type.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, type);
         }
     }
---
+++ NiquIoC/Resolver/FullEmitFunctionResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NiquIoC.Resolver
+{
+    internal class FullEmitFunctionResult
+    {
+        public Func<Dictionary<Type, ContainerMember>, Dictionary<int, Type>, object> Result { get; set; }
+    }
+}
