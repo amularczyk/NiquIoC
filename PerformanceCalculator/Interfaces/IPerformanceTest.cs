58025f2e666d30b026abce0cee89ec09000651e9
amularczyk <xniqux@gmail.com>
2018-01-14 14:10:40 +0100

    Remove MasterThesis and PerformanceCalculator.

--- PerformanceCalculator/Interfaces/IPerformanceTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using PerformanceCalculator.Common;
-
-namespace PerformanceCalculator.Interfaces
-{
-    public interface IPerformanceTest
-    {
-        TestResult RunTest(int count, string TestCase, RegistrationKind registrationKind);
-    }
-}
